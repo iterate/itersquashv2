5394a108d4db4f4b54d6276e032a5261547e2d6b
Magnus Lien <lien_magnus@outlook.com>
2018-01-08 20:25:01 +0100

    Add endpoints, refactor code

--- app/lib/router.js
+++ app/lib/router.js
@@ -1,27 +1,21 @@
 
 'use strict';
 
-const express
-        = require('express'),
-    config
-        = require('../config/config.js'),
-    log
-        = require('./log.js'),
-    bodyParser
-        = require('body-parser'),
-    url
-        = require('url'),
-    mail
-        = require('./mail.js'),
-    models
-        = require('../storage/models'),
-    Promise
-        = require('bluebird'),
-    router
-        = express.Router();
+const express       = require('express');
+const config        = require('../config/config.js');
+const log           = require('./log.js');
+const bodyParser    = require('body-parser');
+const url           = require('url');
+const mail          = require('./mail.js');
+const models        = require('../storage/models');
+const Promise       = require('bluebird');
+const router        = express.Router();
+const path          = require('path');
 
 
-// Log all requests
+/*
+* Log all requests
+*/
 
 router.all('*', (req, res, next) => {
     log.info(`${req.method} ${req.url}`);
@@ -29,123 +23,49 @@ router.all('*', (req, res, next) => {
 });
 
 
-//Client app endpoint
+/*
+* Landing page
+*/
 
-router.get('/:title', (req, res, next) => {
-    models.room.findOrCreate({
-        include: [ models.entry ],
+router.get('/event/:id', (req, res, next) => {
+    models.event.findOrCreate({
+        include: [ models.participant ],
         where: {
-            title: req.params.title.toLowerCase()
+            title: req.params.id.toLowerCase()
         },
         defaults: {
-            description: `# ${req.params.title.toLowerCase()}`
+            description: `# ${req.params.id.toLowerCase()}`
         }
     })
-    .then(() => {
-        return res.status(200).render('index.html');
-    })
-    .catch((err) => {
-        next(err);
-    });
-
-});
-
-
-//Create entry endpoint
-//This doesn't update existing ones yet
-
-router.put('/api/:title/entry', bodyParser.json(), (req, res, next) => {
-    models.room.findOne({
-        include: [ models.entry ],
-        where: {
-            title: req.params.title.toLowerCase()
-        }
-    })
-    .then((roomDAO) => {
-        const entries = roomDAO.dataValues.entries;
-        if(!req.body.name || entries.some((el) => el.name === req.body.name )) {
-            return roomDAO;
-        }
-
-        return models.entry.create({
-            name: req.body.name,
-            roomid: roomDAO.dataValues.id
-        })
-        .then((entry) => {
-            roomDAO.addEntry(entry);
-            return roomDAO;
-        })
-    })
-    .then(() => {
-        return models.room.findOne({
-            include: [ models.entry ],
-            where: {
-                title: req.params.title.toLowerCase()
-            }
-        })
-    })
-    .then((room) => {
-        return res.status(200)
-            .json(room.get({plain: true}));
+    .spread((event) => {
+        return res.status(200).sendFile(path.join(process.cwd(), config.get('docRoot'), '/index.html'));
     })
     .catch((err) => {
         next(err);
     });
 });
 
-//Update room description
 
-router.put('/api/:title/description', bodyParser.json(), (req, res, next) => {
-    models.room.findOne({
+/*
+* Event data
+*/
+
+router.get('/api/:id', (req, res, next) => {
+    models.event.find({
+        include: [ models.participant ],
         where: {
-            title: req.params.title.toLowerCase()
-        }
-    })
-    .then((room) => {
-        return room.update({ description: req.body.description });
-    })
-    .then((room) => {
-        return res.status(200)
-            .json(room.get({plain: true}));
-    })
-    .catch((err) => {
-        next(err);
-    });
-});
-
-router.get('/api/:title/description', (req, res, next) => {
-    models.room.findOne({
-        where: {
-            title: req.params.title.toLowerCase()
-        }
-    })
-    .then((room) => {
-        return room.update({ description: req.query.value });
-    })
-    .then((room) => {
-        return res.status(200)
-            .json(room.get({plain: true}));
-    })
-    .catch((err) => {
-        next(err);
-    });
-});
-
-//Fetch room data endpoint
-
-router.get('/api/:title', (req, res, next) => {
-    models.room.findOrCreate({
-        include: [ models.entry ],
-        where: {
-            title: req.params.title.toLowerCase()
+            title: req.params.id.toLowerCase()
         },
         defaults: {
-            description: `# ${req.params.title.toLowerCase()}`
+            description: `# ${req.params.id.toLowerCase()}`
         }
     })
-    .spread((room) => {
-        return res.status(200)
-            .json(room.get({plain: true}));
+    .then((event) => {
+        if(event) {
+            return res.status(200).json({ title: event.dataValues.title, id: event.dataValues.id });
+        } else {
+            next();
+        }
     })
     .catch((err) => {
         next(err);
@@ -153,37 +73,151 @@ router.get('/api/:title', (req, res, next) => {
 });
 
 
-//Mail endpoint
+/*
+* Create participant
+*/
 
-router.get('/invite', (req, res, next) => {
-    const urlObj = url.parse(req.url, true);
-    const recipients = urlObj.query.recipients;
+router.put('/api/:id/participants', bodyParser.json(), (req, res, next) => {
+    if(!req.body.name) {
+        return res.status(400).json({ error : `Expected a name:String in the request body.`, status: 400 });
+    }
+    
+    return models.event.findOne({
+        include: [ models.participant ],
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(event => {
+        if(!event){
+            return res.status(404).json({ error : `No event with id ${req.params.id} found.`, status: 404 });
+        } else {
+            return event.createParticipant({
+                name: req.body.name,
+                eventid: event.dataValues.id
+            })
+            .then(_ => event.getParticipants({ order: models.sequelize.literal('id ASC') })) //FIXME: {plain: true} returnerer bare 1 objekt
+            .then(participants => res.status(200).json(participants.map(p => p.dataValues)))
+            .catch(next);
+        }
+    });
+});
 
-    if(!recipients) {
-        throw Error("No recipients found. Example: /invite?recipients=foo@baz.com,bar@bizz.com");
+
+/*
+* Update participant
+*/
+
+router.put('/api/:id/participants/:participantId', bodyParser.json(), (req, res, next) => {
+    if (!req.body.name || isNaN(parseInt(req.params.participantId, 10)) || isNaN(parseInt(req.params.id, 10))) {
+        return res.status(400).json({ error : `Invalid request`, status: 400 });
     }
 
-    mail.send(recipients)
-        .then(() => {
-            res.status(200).send("Mail sent successfully!");
-            return;
-        }).catch((err) => {
-            return next(err);
-       });
+    models.participant.update({ name: req.body.name }, { where: { id: req.params.participantId }, returning: true})
+    .then((one, two) => {
+        return models.participant.findAll({ where: { eventid: req.params.id }, order: models.sequelize.literal('id ASC')})
+    }) //FIXME: {plain: true} returnerer bare 1 objekt
+    .then(participants => res.status(200).json(participants.map(p => p.dataValues)))
+    .catch(next);
 });
 
 
-// Health check
+/*
+* Delete participant
+*/
+
+router.delete('/api/:id/participants/:participantId', (req, res, next) => {
+    if (isNaN(parseInt(req.params.participantId, 10)) || isNaN(parseInt(req.params.id, 10))) {
+        return res.status(400).json({ error : `Invalid request`, status: 400 });
+    }
+
+    models.participant.destroy({ where: { id: req.params.participantId }})
+    .then(_ => {
+        return models.participant.findAll({ where: { eventid: req.params.id }, order: models.sequelize.literal('id ASC')})
+    }) //FIXME: {plain: true} returnerer bare 1 objekt
+    .then(participants => res.status(200).json(participants.map(p => p.dataValues)))
+    .catch(next);
+});
+
+
+/*
+* Get participant
+*/
+
+router.get('/api/:id/participants', (req, res, next) => {
+    models.participant.findAll({
+        where: {
+            eventid: req.params.id
+        }
+        , order: models.sequelize.literal('id ASC')
+    })
+    .then((participants) => {
+        return res.status(200)
+            .json(participants.map(p => p.dataValues));
+    })
+    .catch((err) => {
+        next(err);
+    });
+});
+
+
+/*
+* Update event description
+*/
+
+router.put('/api/:id/description', bodyParser.json(), (req, res, next) => {
+    models.event.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then((event) => {
+        return event.update({ description: req.body.description });
+    })
+    .then((event) => {
+        return res.status(200)
+            .json(event.get({plain: true}));
+    })
+    .catch((err) => {
+        next(err);
+    });
+});
+
+
+/*
+* Get event description
+*/
+
+router.get('/api/:id/description', (req, res, next) => {
+    models.event.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then((event) => {
+        return res.status(200)
+            .json(event.dataValues.description);
+    })
+    .catch((err) => {
+        next(err);
+    });
+});
+
+
+/*
+* Health check
+*/
 
 router.get('/ping', (req, res) => {
     res.status(200).send('OK ' + config.get('version'));
 });
 
 
-// Static files
+/*
+* Static files
+*/
 
-router.use('/assets', express.static(config.get('docRoot')));
+router.use('/', express.static(config.get('docRoot')));
 
-// Export application
 
 module.exports = router;
